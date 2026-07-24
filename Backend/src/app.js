a6dde2b5d8d8ed206a2d917aa02b7264e380b9e2
Aditya-is-op200 <adityaanandsingh2004codes@gmail.com>
2026-07-24 23:26:48 +0530

    Setup cors and implemented it in the backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+//credentials need to be true for tokens to be accessed by the server 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes.js");
 
